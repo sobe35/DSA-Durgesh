adb37a152b9b5e2da1563a46684cca1cffbfa1e5
Danish <155543565+sobe35@users.noreply.github.com>
2025-02-26 23:46:23 +0530

    Update CountElements.java

--- Arrays/CountElements.java
+++ Arrays/CountElements.java
@@ -1,8 +1,11 @@
 package Arrays;
-
+import java.util.Arrays;
 public class CountElements {
     public static void main(String[] args) {
+        
         int [] arr = {2,3,4,5,6,7,9,8,1,0};
-        System.out.println(arr.length);
+      //  System.out.println(arr.length);
+      long count=  Arrays.stream(arr).count();
+        System.out.println("The Number of Elements in a given Array are:" + count);
     }
 }
